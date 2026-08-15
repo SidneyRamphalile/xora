9d9e818ad834e9de298b4a76c6e2a8efe079a67e
SidneyRamphalile <72583663+SidneyRamphalile@users.noreply.github.com>
2026-08-16 01:34:13 +0200

    Update Header.jsx

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -37,7 +37,7 @@ const NavLink = ({ title }) => (
 
 
   return (
-    <header className={clsx('fixed top=0 left-0 z-50 w-full py-10 transition-aall duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
+    <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2" href="">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
